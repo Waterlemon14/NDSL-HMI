ffaab5168412b5a42c905d9207963f13693b09e7
Waterlemon14 <aeisenhowerii@gmail.com>
2026-01-23 14:39:30 +0800

    Fix: naming consistency

--- ca/init.sh
+++ ca/init.sh
@@ -7,11 +7,6 @@ openssl genrsa -out root-ca.key 4096
 openssl req -x509 -new -nodes -key root-ca.key \
 -sha256 -days 3650 -config root-ca.cnf -out certs/root-ca.crt # can remove -nodes flag to encrypt private key with password you provide
 
-# 
-# openssl x509 -in certs/ca.crt -out certs/ca.crt -outform PEM
-
-
-# Public copy of public key and CA cert
-# cp public.key ../shared/CAPublic.key
-cp certs/root-ca.crt ../middleware/data/ca.crt
-# cp certs/root-ca.crt ../device/ca.crt
+# Public copy of  CA cert
+cp certs/root-ca.crt ../middleware/data/root-ca.crt
+cp certs/root-ca.crt ../IoT/go/root-ca.crt
--- ca/signClient.sh
+++ ca/signClient.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 # Sign Client Cert
-openssl x509 -req -in client.csr -copy_extensions=copy -CA certs/root-ca.crt -CAkey root-ca.key -CAcreateserial -out certclient.crt -days 365 -sha256
+openssl x509 -req -in client.csr -copy_extensions=copy -CA certs/root-ca.crt -CAkey root-ca.key -CAcreateserial -out certs/client.crt -days 365 -sha256
