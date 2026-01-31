2efcd11d5a8491b9b27a434716d0cfdcf63ae058
chrissenatin <cmsenatin@up.edu.ph>
2026-01-31 22:36:45 +0800

    feat: add device manufacturer to info provided

--- servers/ra/idverification/models.py
+++ servers/ra/idverification/models.py
@@ -6,5 +6,6 @@ class Device(models.Model):
     ip = models.CharField(max_length=30)
     mac = models.CharField(max_length=30, unique=True)
     public_key = models.CharField(max_length=130, blank=True, null=True)
+    manufacturer = models.TextField(blank=False, null=True)
     csr = models.TextField(blank=True, null=True)
     certificate = models.TextField(blank=True, null=True)
