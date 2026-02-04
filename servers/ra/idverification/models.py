327bcd0654a7edafdc2976a32d84956d9585eb80
chrissenatin <cmsenatin@up.edu.ph>
2026-02-04 14:34:44 +0800

    fix: corrected model for current implementation, missed url merging

--- servers/ra/idverification/models.py
+++ servers/ra/idverification/models.py
@@ -3,10 +3,16 @@ from django.db import models
 # Create your models here.
 
 class Device(models.Model):
-    ip = models.CharField(max_length=30)
+    updatedAt = models.DateTimeField(auto_now=True)
+
     mac = models.CharField(max_length=30, unique=True)
+    ip = models.CharField(max_length=30)
+
     public_key = models.CharField(max_length=130, blank=True, null=True)
     manufacturer = models.TextField(blank=False, null=True)
     csr = models.TextField(blank=True, null=True)
+    
+    challengeCount = models.SmallIntegerField(default=0)
+    interval = models.SmallIntegerField(default=1000000)
+
     certificate = models.TextField(blank=True, null=True)
-    last_active = models.DateTimeField(blank=True, null=True)
