693c5eda905b5e560ed1093663052244a003dd93
Waterlemon14 <aeisenhowerii@gmail.com>
2026-02-14 11:57:26 +0800

    ADD: device state class

--- servers/ra/idverification/models.py
+++ servers/ra/idverification/models.py
@@ -2,6 +2,17 @@ from django.db import models
 
 # Create your models here.
 
+class State:
+    CONNECTED = 'connected'
+    RECONNECTING = 'reconnecting'
+    SUSPENDED = 'suspended'
+
+    CHOICES = [
+        (CONNECTED, 'Connected'),
+        (RECONNECTING, 'Reconnecting'),
+        (SUSPENDED, 'Suspended'),
+    ]
+
 class Device(models.Model):
     updatedAt = models.DateTimeField(auto_now=True)
 
@@ -16,3 +27,9 @@ class Device(models.Model):
     interval = models.SmallIntegerField(default=1000000)
 
     certificate = models.TextField(blank=True, null=True)
+
+    state = models.CharField(
+        max_length=20,
+        choices=State.CHOICES,
+        default=State.CONNECTED,
+    )
