d806dd2249c55dc91e87e90918678ea2e54ce3d4
chrissenatin <cmsenatin@up.edu.ph>
2026-02-22 16:38:00 +0800

    feat: filter out devices with certificates and owners when requesting cert

--- servers/ra/idverification/helper.py
+++ servers/ra/idverification/helper.py
@@ -3,7 +3,7 @@ import ipaddress
 from idverification.models import Device
 
 def get_select_list(request):
-    devices = Device.objects.all()
+    devices = Device.objects.all().filter(certificate__isnull=True, owner__isnull=True)
     client_ip = int(ipaddress.ip_address(request.META.get('REMOTE_ADDR')))
     device_match = []
     for device in devices:
@@ -12,8 +12,6 @@ def get_select_list(request):
         device_match.append((device, matched_prefix))
     
     device_match.sort(key=lambda x: x[1], reverse=True)
-    print(request.META.get('REMOTE_ADDR'))
-    for x in device_match: print(x[0].ip, x[1])
     sorted_devices = [dev for dev, matched in device_match]
     
     likely = sorted_devices [:5]
