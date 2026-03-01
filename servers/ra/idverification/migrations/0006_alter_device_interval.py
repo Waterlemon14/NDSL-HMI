83e60a9e3265157594ed2220a9f240ac888fc7a5
chrissenatin <cmsenatin@up.edu.ph>
2026-03-02 00:11:58 +0800

    fix: changed conflicting migration names

--- servers/ra/idverification/migrations/0006_alter_device_interval.py
+++ servers/ra/idverification/migrations/0006_alter_device_interval.py
@@ -6,7 +6,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('idverification', '0004_alter_device_state'),
+        ('idverification', '0005_notification'),
     ]
 
     operations = [
